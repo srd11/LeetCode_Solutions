3f6c83a46b5eb978339ff84cba2d5cad9e365f42
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2022-10-04 01:39:15 +0530

    Time: 8 ms (25.97%), Space: 8.6 MB (89.64%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,33 +1,19 @@
-void distribute(vector<int>& students,vector<int>& sandwiches){
-    for(int i=0;i<students.size();i++){
-        if(students[i]==sandwiches[0]){
-            students.erase(students.begin()+i);
-            sandwiches.erase(sandwiches.begin());
-            return;
-        }
-    }
-    return;
-}
-
-
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        
-         while(students.size()!=0){
-             int flag=0;
-             for(int i=0;i<students.size();i++){
-                 if(students[i]==sandwiches[0]){     
-                 flag=1;
-                 distribute(students,sandwiches);
-                 break;
-                 }
-             }
-             if(flag==0){
-                 return students.size();
-             }
-             flag=0;
-         } 
-        return students.size();
+        int preferenceCount[]={0,0};
+    int eaten=0;
+    for(int student:students) preferenceCount[student]++;
+    for(int sandwich:sandwiches)
+    {
+        if(preferenceCount[sandwich]>0)
+        {
+            eaten++;
+            preferenceCount[sandwich]--;
+        }
+        else break;
     }
+    return (students.size()-eaten);
+    }
+    
 };
