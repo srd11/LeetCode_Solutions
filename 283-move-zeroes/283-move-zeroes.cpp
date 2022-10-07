da3fe18e87933b006f58ec69df3062864be0709d
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2022-10-07 16:05:09 +0530

    Time: 52 ms (38.26%), Space: 19.1 MB (83.08%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -3,20 +3,28 @@ public:
     void moveZeroes(vector<int>& nums) {
        vector<int>::iterator it;
         
-            int j=nums.size();
-            int i=0;
-            while(j!=0)
-            {
-                /* code */
-                if(nums[i]==0 && i<nums.size()){
-                it = nums.begin()+i;
-                nums.erase(it);
-                nums.push_back(0);
-                // cout<<"Hii ";
-                }
-                else
-                    i++;
-                j--;
+//             int j=nums.size();
+//             int i=0;
+//             while(j!=0)
+//             {
+//                 /* code */
+//                 if(nums[i]==0 && i<nums.size()){
+//                 it = nums.begin()+i;
+//                 nums.erase(it);
+//                 nums.push_back(0);
+    
+//                 }
+//                 else
+//                     i++;
+//                 j--;
+//             }
+        
+        
+        int i=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j] !=0){
+                swap(nums[j],nums[i++]);
             }
+        }
     }
 };
