16da049fc6413d882a1e4ede1b7907504cc38df6
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2022-10-10 22:44:51 +0530

    Update 1328-break-a-palindrome.cpp

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,42 +1,7 @@
 class Solution {
 public:
     string breakPalindrome(string palindrome) {
-        
-//         int flag=0,n=palindrome.length();
-     
-//             if(n==0 || n==1)
-//                 return "";
-//           n--;
-//         if((n==2)&&(palindrome[n]!=palindrome[n-1])){
-//             palindrome[n]=palindrome[n-1];
-//             flag=1;
-//         }
-       
-//         for(int i=1;i<=n;i++){
-            
-//             // if((i==n-1) && (palindrome[i]!=palindrome[i+1])){
-//             //      palindrome[i+1]=palindrome[i];
-//             //     flag=1;
-//             // }
-//             if((palindrome[i]!=palindrome[i-1])&&flag==0){
-//                  palindrome[i]=palindrome[i-1];
-//                 flag=1;
-//                 break;
-//             }
-               
-//         }
-        
-        
-//         //      if((palindrome[n-2]=palindrome[n-1])&& flag==0){
-//         //          palindrome[n-1]=palindrome[n-2];
-//         //          flag==1;
-//         //      }
-//         if(flag==0){
-//             char ch=palindrome[n];
-//             ch++;
-//             palindrome[n]=ch;
-//         }
-//         return palindrome;
+              
         
         int n=palindrome.length();
         int m=n/2;
