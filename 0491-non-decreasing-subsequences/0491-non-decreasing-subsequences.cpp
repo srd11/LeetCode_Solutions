dc3f93a0cea28ae09b38dd68c1257f5852a2ab44
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2023-01-21 15:40:40 +0530

    Time: 71 ms (72.70%), Space: 22.1 MB (81.74%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,27 +1,26 @@
 class Solution {
-private:
-    void findSubsequences(vector<int> &nums, vector<int> &subsequence,
-                        set<vector<int> > &result, int size, int index) {
-    if (size >= 2)
-        result.insert(subsequence);
-
-
-    for (int i = index; i < nums.size(); ++i) {
-        if (subsequence.size() == 0 || nums[i] >= subsequence[subsequence.size() - 1]) {
-            subsequence.push_back(nums[i]);
-            findSubsequences(nums, subsequence, result, size + 1, i + 1);
-            subsequence.pop_back();
-        }
-    }
-}
-
 public:
+    
+    void subsequences(vector<int> &nums,vector<int> &subsequence, set<vector<int>> &ansSet,int size,int index){
+        if(size >= 2)
+            ansSet.insert(subsequence);
+            
+        for(int i=index;i<nums.size();i++){
+            if(subsequence.size() == 0 || nums[i] >= subsequence[subsequence.size()-1]){
+                subsequence.push_back(nums[i]);
+                subsequences(nums,subsequence,ansSet,size+1,i+1);
+                subsequence.pop_back();
+            }
+        }
+        
+        
+    }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        set<vector<int>> resultSet;
+        set<vector<int>> ansSet;
+        int n = nums.size();
         vector<int> subsequence;
-        findSubsequences(nums, subsequence, resultSet, 0, 0);
-
-        vector<vector<int>> result(resultSet.begin(), resultSet.end());
-        return result;
+        subsequences(nums,subsequence,ansSet,0,0);
+        vector<vector<int>> ans(ansSet.begin(),ansSet.end());
+        return ans;
     }
 };
