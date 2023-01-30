208db46ffaff8ef8379147d8ce5c8457a45e5ba3
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2023-01-30 11:23:46 +0530

    Time: 0 ms (100.00%), Space: 6 MB (37.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    long long int arr[38]={0};
     int tribonacci(int n) {
+        int tn0=0,tn1=1,tn2=1;
+        int tn=2;
         
         if(n == 0)
             return 0;
-        if(n == 1)
+        if(n == 1 || n == 2)
             return 1;
-        if(n == 2)
-            return 1;
-        if(arr[n] != 0)
-            return arr[n];
-        return arr[n] = tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        while(tn != n){
+            int num = tn2+tn1+tn0;
+            tn0=tn1;
+            tn1=tn2;
+            tn2=num;
+            tn++;
+        }
+        return tn2;
     }
 };
-
