4a69a3c4d87cd333e7c0c8824bd0c9f6ef5b955e
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2023-02-17 16:40:59 +0530

    Time: 4 ms (56.01%), Space: 10 MB (6.62%) - LeetHub

---
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+//   public:
+//     int minDiffInBST(TreeNode *root) {
+//         int res = INT_MAX, pre = -1;
+//         helper(root, pre, res);
+//         return res;
+//     }
+
+//     void helper(TreeNode *root, int &pre, int &res) {
+//         if (root) {
+//             helper(root->left, pre, res);
+//             if (pre != -1) {
+//                 res = min(res, root->val - pre);
+//             }
+//             pre = root->val;
+//             helper(root->right, pre, res);
+//         }
+//     }
+// };
+
+class Solution {
+  public:
+    
+    vector<int> inorder;
+    
+    void inOrderTraversal(TreeNode *root){
+        
+        if(root == NULL)
+            return;
+        
+        inOrderTraversal(root->left);
+        inorder.push_back(root->val);
+        inOrderTraversal(root->right);
+        
+    }
+    int minDiffInBST(TreeNode *root) {
+        inOrderTraversal(root);
+        int res =INT_MAX;
+        
+        for(int i=0;i<inorder.size()-1;i++){
+            res = min(inorder[i+1]-inorder[i],res);
+        }
+        return res;
+    }
+    
+};
+
+
