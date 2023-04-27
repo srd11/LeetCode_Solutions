22b78418e977656b1fe83c20c4958195c3434669
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2023-04-28 00:27:49 +0530

    Time: 0 ms (100.00%), Space: 6 MB (63.99%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+    int counts = 0;
+    
+    for (int i=1; i*i<=n; ++i) {
+        ++ counts;    
+    }
+    
+    return counts;
+}
+
+};
