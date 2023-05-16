d64e9260dc0614f6d1dfca77cbbcaf38fb1e25e2
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2023-05-16 22:56:08 +0530

    Time: 709 ms (25.60%), Space: 179.9 MB (99.96%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -11,16 +11,18 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-    ListNode *n1 = nullptr, *n2 = nullptr;
-    for (auto p = head; p != nullptr; p = p->next) {
-        n2 = n2 == nullptr ? nullptr : n2->next;
-        if (--k == 0) {
-            n1 = p;
-            n2 = head;
+        ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
+        while (--k)
+            ptr1 = ptr1->next;
+        
+        kth = ptr1;
+        ptr1 = ptr1->next;
+        
+        while (ptr1) {
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
         }
+        swap(ptr2->val, kth->val);
+        return head;
     }
-	// The problem description specifically asks to swap values, not nodes themselves. 
-    swap(n1->val, n2->val);
-    return head;
-}
 };
