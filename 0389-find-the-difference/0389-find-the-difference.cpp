2e3f8e1da73e94e0d0125428eb5f3b32b41fb13b
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2023-09-25 22:05:57 +0530

    Time: 7 ms (5.95%), Space: 7.2 MB (8.68%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        if(s=="") return t[0];
+        
+        map<char, int> m;
+        for(char ch: s){
+            m[ch]++;
+        }
+        char ans;
+        for(char ch: t){
+            if(m[ch]>0){
+                m[ch]--;
+            }
+            else{
+                return ch;
+            }
+        }
+        return '\0';
+    }
+};
