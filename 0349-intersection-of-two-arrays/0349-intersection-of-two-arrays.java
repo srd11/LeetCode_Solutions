1029fa6a9f9acfe821f8fb88ed3ba673fd5ecc5d
Shruti Devshatwar <66111363+srd11@users.noreply.github.com>
2024-03-10 17:45:46 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (61.49%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        
+        final int N = 1001;
+        var set = new int [N];
+        for (var i : nums1) set[i] = 1;
+        int resCount = 0;
+        for (var i : nums2) {
+            if (set[i] == 1) {
+                set[i] = 2;
+                ++resCount;
+            }
+        }
+        var res = new int[resCount];
+        var i = 0;
+        var nums = nums2.length > nums1.length ? nums1 : nums2;
+        for (var v : nums) {
+            if (set[v] == 2) {
+                res[i++] = v;
+                set[v] = 1;
+            }
+        }
+        return res;
+    }
+}
